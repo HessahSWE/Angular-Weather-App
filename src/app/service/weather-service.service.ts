0c7c643d639379118131530a79f25f52cf9eeb49
HessahSWE <hissahalhussain@gmail.com>
2022-10-13 00:29:57 +0300

    update

--- src/app/service/weather-service.service.ts
+++ src/app/service/weather-service.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherServiceService {
 
-  constructor( private Http: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string) : Observable<any> {
+     return this.http.get(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders().set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue).
+      set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+       params: new HttpParams().
+      set('q', cityName).
+      set('units', 'metric').
+      set('mode', 'json')
+    });
+
+  }
 }
+
 
